cf1ac4cab98dc78afe556f7b1178b123672de047
chansung park <deep.diver.csp@gmail.com>
2018-04-25 16:51:39 +0900

    adding a handy Foursquare API fetch - venue search - get venue photo

---
+++ app/src/FSAPIs.js
@@ -0,0 +1,11 @@
+const api = "https://api.foursquare.com/v2"
+const client_id = "GD1U0GHJVRDG1HNUQMPZVIA0GFPV2TSATLU5BEXQDBT4T2LM"
+const client_secret = "PZXI0GPI45F00XFPDLHHSUN0EMFYZF4OWLW2FZA53IL2OWKR"
+
+export const search = (near) =>
+  fetch(`${api}/venues/explore?near=${near}&client_id=${client_id}&client_secret=${client_secret}`)
+  .then(res => res.json())
+
+export const getPhoto = (venueId) =>
+  fetch(`${api}/venues/${venueId}/photos?&client_id=${client_id}&client_secret=${client_secret}`)
+  .then(res => res.json())

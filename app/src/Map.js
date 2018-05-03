83b48ace5d5e5fdc1566f2972fc60b3f3f7502f5
chansung park <deep.diver.csp@gmail.com>
2018-05-04 01:52:57 +0900

    put SearchBox in the map - /*global google*/ is required when you face "error 'google' is not defined no-undef" (https://github.com/tomchentw/react-google-maps/issues/434) - need to put libraries=places in the URL

--- app/src/Map.js
+++ app/src/Map.js
@@ -1,7 +1,10 @@
+/*global google*/
+
 import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import SearchBox from "react-google-maps/lib/components/places/SearchBox";
 import './Map.css';
-import MapStyleOptions from './MapStyleOptions.json'
+import MapStyleOptions from './MapStyleOptions.json';
 
 class Map extends Component {
    render() {
@@ -11,6 +14,29 @@ class Map extends Component {
           defaultZoom = { 8 }
           defaultOptions = { {styles: MapStyleOptions} }
         >
+          <SearchBox
+            ref={props.onSearchBoxMounted}
+            bounds={props.bounds}
+            controlPosition={google.maps.ControlPosition.TOP_LEFT}
+            onPlacesChanged={props.onPlacesChanged}>
+            <input
+              type="text"
+              placeholder="Customized your placeholder"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                marginTop: `27px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+              }}
+            />
+          </SearchBox>
         </GoogleMap>
      ))
 

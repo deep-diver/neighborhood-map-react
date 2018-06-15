23a329344eb3f999b7bbeb97d615fca782cd4625
chansung park <deep.diver.csp@gmail.com>
2018-06-15 14:05:27 +0900

    let side-menu component displays enriched contents. - phone number, address, name, photo, number of likes for each venue. - different colour theme is applied.

--- app/src/SideMenu.js
+++ app/src/SideMenu.js
@@ -3,6 +3,10 @@ import TimerMixin from 'react-timer-mixin'
 import './SideMenu.css'
 import * as FSAPI from './FSAPIs'
 
+import like from './like.png'
+import phone from './phone.png'
+import address from './address.png'
+
 class SideMenu extends Component {
   timeout = null
 
@@ -12,12 +16,12 @@ class SideMenu extends Component {
   }
 
   openSideMenu() {
-    document.querySelector('#mySidenav').style.width = "350px"
-    document.querySelector("#top-bar").style.marginLeft = "350px"
-    document.querySelector("#map").style.marginLeft = "350px"
+    document.querySelector('#mySidenav').style.width = "400px"
+    document.querySelector("#top-bar").style.marginLeft = "400px"
+    document.querySelector("#map").style.marginLeft = "400px"
 
     this.timeout = TimerMixin.setTimeout(() => {
-      document.querySelector("#map").style.width = "calc(100% - 350px)"
+      document.querySelector("#map").style.width = "calc(100% - 400px)"
 
       TimerMixin.clearTimeout(this.timeout)
     }, 350);
@@ -32,26 +36,6 @@ class SideMenu extends Component {
 
   render() {
     let {radius, limits, center, venues, onVenueUpdated, onVenueSelected, onRadiusChanged, onLimitsChanged} = this.props
-
-    // const promises = []
-    // for (const venue of venues) {
-    //   promises.push(FSAPI.getPhotos(venue.id))
-    // }
-    //
-    // Promise.all(promises).then(function(photoDatas) {
-    //   for (const photoData of photoDatas) {
-    //     const photos = photoData.response.photos
-    //
-    //     if (photos.count > 0 && photos.items.length > 0) {
-    //       const photo = photos.items[0]
-    //       const prefix = photo.prefix
-    //       const suffix = photo.suffix
-    //
-    //       const url = prefix + "100x100" + suffix
-    //       venue.thumbnail = url
-    //     }
-    //   }
-    // })
 
     return (
       <div id="mySidenav" className="sidenav">
@@ -83,14 +67,40 @@ class SideMenu extends Component {
                 className="venue"
                 key={venue.id}
                 onClick={onVenueSelected.bind(this, index)}>
-                <p> {venue.name} </p>
+                <p className="venue-title"> {venue.name} </p>
                 <img
                   src={
-                    (venue.photos && venue.photos.groups && venue.photos.groups[0].items ?
+                    (venue.photos ? venue.photos.groups ? venue.photos.groups[0] ? venue.photos.groups[0].items ?
                       venue.photos.groups[0].items[0].prefix + "100x100" + venue.photos.groups[0].items[0].suffix :
-                      "https://dummyimage.com/100x100/ffffff/fff&text=no+image")
+                      "https://dummyimage.com/100x100/ffffff/fff&text=no+image" : "https://dummyimage.com/100x100/ffffff/fff&text=no+image" : "https://dummyimage.com/100x100/ffffff/fff&text=no+image" : "https://dummyimage.com/100x100/ffffff/fff&text=no+image" )
                   }
                   alt="venue image" />
+                <div className="venue-info">
+                  <div>
+                    <img src={like}/>
+                    <span className="venue-info-value">
+                    { venue.likes ? venue.likes.count > 0 ?
+                      venue.likes.summary :
+                      'Not Yet Registered' : 'Not Yet Registered' }
+                    </span>
+                  </div>
+                  <div>
+                    <img src={phone}/>
+                    <span className="venue-info-value">
+                    {venue.contact ? venue.contact.formattedPhone ?
+                      venue.contact.formattedPhone :
+                      'Not Yet Registered' : 'Not Yet Registered'}
+                    </span>
+                  </div>
+                  <div>
+                    <img src={address}/>
+                    <span className="venue-info-value">
+                      {venue.location ? venue.location.city ?
+                        venue.location.city + ", " + venue.location.address :
+                        'Not Yet Registered' : 'Not Yet Registered'}
+                    </span>
+                  </div>
+                </div>
               </div>
             )
           }

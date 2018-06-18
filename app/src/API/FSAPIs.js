32d5994770af5972c79fc1d444e609f62e27d200
chansung park <deep.diver.csp@gmail.com>
2018-06-19 00:12:10 +0900

    adding catch for each APIs

--- app/src/API/FSAPIs.js
+++ app/src/API/FSAPIs.js
@@ -21,6 +21,7 @@ Travel & Transport (4d4b7105d754a06379d81259)
 export const search = (lat, lng, radius, limits, query) =>
   fetch(`${api}/venues/search?ll=${lat},${lng}&limit=${limits}&radius=${radius}&query=${query}&client_id=${client_id}&client_secret=${client_secret}&v=${version}`)
   .then(res => res.json())
+  .catch(error => alert(error + ' on search API call'))
 
 export const search_q = (lat, lng, query, radius, limits, categories) =>
   fetch(`${api}/venues/search?ll=${lat},${lng}&
@@ -31,11 +32,14 @@ export const search_q = (lat, lng, query, radius, limits, categories) =>
                               client_secret=${client_secret}&
                               v=${version}`)
   .then(res => res.json())
+  .catch(error => alert(error + ' on search_q API call'))
 
 export const getDetail = (venueId) =>
   fetch(`${api}/venues/${venueId}?&client_id=${client_id}&client_secret=${client_secret}&v=${version}`)
   .then(res => res.json())
+  .catch(error => alert(error + ' on getDetail API call'))
 
 export const getPhotos = (venueId) =>
   fetch(`${api}/venues/${venueId}/photos?&limit=1&client_id=${client_id}&client_secret=${client_secret}&v=${version}`)
   .then(res => res.json())
+  .catch(error => alert(error + ' on getPhotos API call'))
